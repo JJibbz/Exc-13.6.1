f0505ac9b5067dc2168b5f923451c8087e7c19b9
JJibbz <vkazancev322@gmail.com>
2024-02-25 17:31:36 +0700

    Решение итогового задания 13.6.1 модуля 13.

--- Exc 13.6.1/Program.cs
+++ Exc 13.6.1/Program.cs
@@ -8,6 +8,13 @@ namespace Exc_13._6._1
         {
             string text = File.ReadAllText(@"C:\Users\lino-\OneDrive\Рабочий стол\Text1.txt");
 
+            GetListTime(text);
+
+            GetLinkedListTime(text);
+        }
+
+        static void GetListTime(string text)
+        {
             var words = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
             List<string> list = new List<string>();
             var watch = new Stopwatch();
@@ -17,6 +24,19 @@ namespace Exc_13._6._1
                 list.Add(word);
             }
             Console.WriteLine($"Вставка в List<T>: {watch.Elapsed.TotalMilliseconds} мс");
+        }
+
+        static void GetLinkedListTime(string text)
+        {
+            var words = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            LinkedList<string> list = new LinkedList<string>();
+            var watch = new Stopwatch();
+            foreach (string word in words)
+            {
+                watch = Stopwatch.StartNew();
+                list.AddLast(word);
+            }
+            Console.WriteLine($"Вставка в LinkedList<T>: {watch.Elapsed.TotalMilliseconds} мс");
         }
     }
 }
