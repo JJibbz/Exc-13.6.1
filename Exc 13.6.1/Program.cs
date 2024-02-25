a66c8003c53a4054dfa33f3416c1b5376c5e3374
JJibbz <vkazancev322@gmail.com>
2024-02-25 17:24:35 +0700

    Добавьте файлы проекта.

---
+++ Exc 13.6.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace Exc_13._6._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText(@"C:\Users\lino-\OneDrive\Рабочий стол\Text1.txt");
+
+            var words = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            List<string> list = new List<string>();
+            var watch = new Stopwatch();
+            foreach (string word in words)
+            {
+                watch = Stopwatch.StartNew();
+                list.Add(word);
+            }
+            Console.WriteLine($"Вставка в List<T>: {watch.Elapsed.TotalMilliseconds} мс");
+        }
+    }
+}
